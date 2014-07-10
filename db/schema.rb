bba147c760e5c76f69903923b2801dc46842f6a5
Glenn Van Bael <glenn@digital-ad.be>
2014-07-10 11:18:28 +0200

    Form update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710090548) do
+ActiveRecord::Schema.define(version: 20140710090915) do
 
   create_table "products", force: true do |t|
     t.string   "name"
